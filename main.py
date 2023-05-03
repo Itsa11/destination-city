11679146df475a534bead10eeb2d113d539d7908
Tanmay Bodakhe <76662202+Itsa11@users.noreply.github.com>
2023-05-03 17:32:19 +0530

    yes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from pyodide.http import open_url 
 
-data = pd.read_csv(open_url("https://raw.githubusercontent.com/Aditya-Holkar/destiny/main/Destiny3.csv"))
+data = pd.read_csv(open_url("https://raw.githubusercontent.com/Itsa11/destination-city/main/Destiny.csv"))
 
 data['Rating']=data['Rating'].apply(str)
 
